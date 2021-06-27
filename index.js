32b096fb609a521cec4b5c0c5ef728ad41eddfb2
Min <mtan121@aucklanduni.ac.nz>
2021-06-27 15:24:57 +0800

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Add your code here
+function submitData(username, useremail) {
+    return fetch('http://localhost:3000/users', {
+        method: 'POST',
+        headers:{
+            'Content-Type': "application/json",
+            'Accept': "application/json",
+        },
+        body: JSON.stringify({
+            name: username,
+            email:useremail,
+        }),
+    })
+      .then(resp => resp.json())
+      .then(json => {
+          const elem = document.createElement('p')
+          elem.textContent = json.id
+          document.querySelector('body').appendChild(elem)
+        })
+        .catch(err => {
+            const elem = document.createElement('p')
+            elem.textContent = err
+            document.querySelector('body').appendChild(elem)
+        })
+}
